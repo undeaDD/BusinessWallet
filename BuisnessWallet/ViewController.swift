589c5fd5f5be638d7adf4ad77321d265d366c89e
Dominic Drees <dominic.drees@atino.de>
2020-07-13 08:29:47 +0200

    Initial Commit

---
+++ BuisnessWallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BuisnessWallet
+//
+//  Created by Dominic Drees on 13.07.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
